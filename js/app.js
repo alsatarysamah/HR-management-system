de9f50aa36eebdc72f7c0ea7ca637ca8635f72ed
samah anwer alsatary <alsatary.samah@yahoo.com>
2022-04-21 18:13:50 +0300

    add event

--- js/app.js
+++ js/app.js
@@ -1,97 +1,118 @@
 'use strict';
-function Employee (employeeID, fullName , department ,level,image,salary){
-this.employeeID=employeeID;
-this.fullName= fullName;
-this.department =department;
-this.level=level;
-this.image=image;
-this.salary=salary;
-}                                                                                                 
+let empArr = [];
+function Employee(employeeID, fullName, department, level, image, salary) {
+    this.employeeID = employeeID;
+    this.fullName = fullName;
+    this.department = department;
+    this.level = level;
+    this.image = image;
+    this.salary = salary;
+    empArr.push(this);
+}
 
-Employee.prototype.calcSallary =function()
-{this.salary=0;
-   
- 
-       if(this.level==="Senior")
-       {
-        this.salary =  getRndInteger(1500,2000);
+Employee.prototype.calcSallary = function () {
+    this.salary = 0;
+
+
+    if (this.level === "Senior") {
+        this.salary = getRndInteger(1500, 2000);
+
+    }
+
+
+
+    if (this.level == "Mid-Senior")
+        this.salary = getRndInteger(1000, 1500);
+
+
+
+    if (this.level == "Junior")
+        this.salary = getRndInteger(500, 1000);
+
+
+}
+
+//render
+let header = document.getElementById("header1");
+let sec = document.getElementById("sec");
+Employee.prototype.printing = function () {
+
+    for (var i = 0; i < empArr.length; i++) {
+        var imageEl = new Image(); 
+        imageEl.src = empArr[i].image;
+
+       //  document.getElementById('container').appendChild(image);
+        // let imageEl = document.createElement("img");
+        // imageEl.src = empArr[i].image;
+        // console.log(+empArr[i].image);
+         sec.appendChild(imageEl);
+
+        let fullName = document.createElement("p");
+        fullName.textContent = empArr[i].fullName ;
+        sec.appendChild(fullName);
+
+
         
-       }
-      
-   
-      
-        if(this.level=="Mid-Senior")
-        this.salary =  getRndInteger(1000,1500);
-    
-   
+        let departmentAndLevel = document.createElement("p");
+        departmentAndLevel.textContent = "Department : "+empArr[i].department +"      "+"Level :"+empArr[i].level ;
+        sec.appendChild(departmentAndLevel);
 
-        if(this.level=="Junior")
-        this.salary =  getRndInteger(500,1000);
-    
+        let empID = document.createElement("p");
+        empID.textContent = empArr[i].employeeID;
+        sec.appendChild(empID);
+
+
+        // let department = document.getElementById("department");
+        // department.textContent = empArr[i].department;
+        // sec.appendChild(department);
+
+        
+        
+        
+    }
+
+    // document.write( this.fullName);
+    // document.write("    ");
+    // document.write( this.salary);
+
+}
+Employee.prototype.netSallary = function () {
+    let tax = .0075 * this.salary;
+
+    this.salary = this.salary - tax;
+
 
 }
 
-
-Employee.prototype.printing =function()
-{
-    document.write( this.fullName);
-    document.write("    ");
-    document.write( this.salary);
-
-}
-Employee.prototype.netSallary=function()
-{ 
-    let tax =.0075*this.salary;
-   
-    this.salary=this.salary-tax;
-
-
-}
-
-//document.getElementById("ss").innerHTML ="my name";
-
-let e1= new Employee(1000,"Ghazi Samer","Administration","Senior","./assest/logoipsum-logo-36.svg",5);
-let e2 = new Employee(1001,"Lana Ali"	,"Finance"	,"Senior","./assest/logoipsum-logo-36.svg",0	);
-let e3 = new Employee(1002,"Tamara Ayoub"	,"Marketing"	,"Senior","./assest/logoipsum-logo-36.svg",0	);
-let e4 = new Employee(1003,"Safi Walid"	,"Administration"	,"Mid-Senior","./assest/logoipsum-logo-36.svg",0	);
-let e5 = new Employee(1004,"Omar Zaid"	,"Development"	,"Senior","./assest/logoipsum-logo-36.svg",0	);
-let e6 = new Employee(1005,"Rana Saleh"	,"Development"	,"Junior","./assest/logoipsum-logo-36.svg",0	);
-let e7 = new Employee(1006,"Hadi Ahmad"	,"Finance"	,"Mid-Senior","./assest/logoipsum-logo-36.svg",0	);
-		
-e1.calcSallary();
-e1.netSallary();
-e1.printing();
-
-e2.calcSallary();
-e2.netSallary();
-e2.printing();
-
-e3.calcSallary();
-e3.netSallary();
-e3.printing();
-
-
-e4.calcSallary();
-e4.netSallary();
-e4.printing();
-
-e5.calcSallary();
-e5.netSallary();
-e5.printing();
-
-e6.calcSallary();
-e6.netSallary();
-e6.printing();
-
-e7.calcSallary();
-e7.netSallary();
-e7.printing();
 
 
 
 
 //functions
 function getRndInteger(min, max) {
-    
-    return Math.floor(Math.random() * (max - min) ) + min;
-  }
+
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
+let formEl = document.getElementById("formid");
+formEl.addEventListener("submit", submition);
+
+
+function submition(event) {
+    event.preventDefault();
+    let fullName = event.target.fname.value;
+    let department = event.target.department.value;
+    let level = event.target.level.value;
+    let imgURL = event.target.img1.value;
+    console.log(department);
+    let newEmp = new Employee(generateID(), fullName, department, level, imgURL, 0);
+    newEmp.printing();
+    newEmp.calcSallary();
+    newEmp.netSallary();
+
+}
+function generateID() {
+    var val = Math.floor(1000 + Math.random() * 9000);
+    return val;
+}
